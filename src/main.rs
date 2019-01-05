d4e87d18971314587d0747c8572c6893221c9b50
Michał Budzyński <michal@virtualdesign.pl>
2019-01-05 19:38:00 +0100

    :grapes: drawing test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 #[macro_use]
 extern crate stdweb;
 
-mod canvcas;
+mod canvas;
 mod direction;
+
+use canvas::Canvas;
 
 fn main() {
     stdweb::initialize();
 
+    let canvas = Canvas::new("#canvas", 20, 20);
 
     stdweb::event_loop();
 }
