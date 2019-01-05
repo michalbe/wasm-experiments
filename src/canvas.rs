2c9b6cce9beebe110e6d85f38c4ee50522864f4e
Michał Budzyński <michal@virtualdesign.pl>
2019-01-05 19:26:00 +0100

    :fuelpump: Canvas struct

--- src/canvas.rs
+++ src/canvas.rs
@@ -2,3 +2,14 @@ use stdweb::traits::*;
 use stdweb::unstable::TryInto;
 use stdweb::web::html_element::CanvasElement;
 use stdweb::web::{document, CanvasRenderingContext2d};
+
+
+pub struct Canvas {
+    pub canvas: CanvasElement,
+    pub ctx: CanvasRenderingContext2d,
+    scaled_width: u32,
+    scaled_height: u32,
+    width: u32,
+    height: u32,
+
+}
