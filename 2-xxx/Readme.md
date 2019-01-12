588c2d8623ae10e57c32e733cb52e3b407cdcba4
Michał Budzyński <michal@virtualdesign.pl>
2019-01-12 01:53:00 +0100

    :ring: second example

---
+++ 2-xxx/Readme.md
@@ -0,0 +1,17 @@
+Wasm game example
+---
+
+Simple web assembly POC in Rust.
+
+## Run localy
+```
+cargo web start --target wasm32-unknown-unknown
+```
+
+## Build
+```
+cargo web build --release --target wasm32-unknown-unknown
+```
+
+## Play online
+[Play here](https://michalbe.github.io/wasm-game/1-simple-rectangle/static/)
---
+++ 2-xxx/src/main.rs
@@ -0,0 +1,44 @@
+extern crate stdweb;
+extern crate wee_alloc;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+mod canvas;
+
+use canvas::Canvas;
+
+use stdweb::traits::*;
+use stdweb::web::{event::KeyDownEvent, IEventTarget};
+
+fn main() {
+    stdweb::initialize();
+
+    let mut x = 45;
+    let mut y = 45;
+
+    let width = 20;
+    let height = 20;
+
+    let canvas = Canvas::new("#canvas", width, height);
+
+    canvas.clear_all();
+    canvas.draw(x, y, "red");
+
+    stdweb::web::document().add_event_listener({
+        move |event: KeyDownEvent| {
+            match event.key().as_ref() {
+                "ArrowLeft" => { x = x - 1},
+                "ArrowRight" => { x = x + 1},
+                "ArrowDown" => { y = y + 1 },
+                "ArrowUp" => { y = y - 1 },
+                _ => {}
+            };
+
+            canvas.clear_all();
+            canvas.draw(x, y, "red");
+        }
+    });
+
+    stdweb::event_loop();
+}
